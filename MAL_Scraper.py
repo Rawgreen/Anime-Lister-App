2a04f88c2361e55ca28b295fe110e93ce965cb3d
Rawgreen <mhali.varol@gmail.com>
2024-04-20 20:29:41 +0300

    More modular code

--- MAL_Scraper.py
+++ MAL_Scraper.py
@@ -18,6 +18,170 @@ class MALProfile:
 
         self.__class_dict.setdefault("Profile URL", self.url)
         self.__class_dict.setdefault("Message", self.__message)
+        self._website_response = requests.get(self.url)
+
+        if self._website_response:
+            self.__class_dict.setdefault("Status Code", self._website_response.status_code)
+            self.soup = BeautifulSoup(self._website_response.text, "html.parser")
+        else:
+            raise Exception(f"Non-success status code: {self._website_response.status_code}")
+
+    def profile_name(self):
+        """Extracts the profile name from the provided URL and stores it in the class dictionary.
+
+        This method attempts to extract the profile name using a regular expression pattern (`([^/]+)$`).
+        If the pattern matches the URL, it captures the last part (assumed to be the profile name)
+        and stores it in the class dictionary under the key "Profile Name".
+        If the pattern fails to match,
+        an error message indicating the pattern was not found
+        is stored in the dictionary under the key "Message".
+        The method then exits.
+
+        **Raises:**
+
+            Exception:
+            (Implicitly raised by `exit()` call) An exception exits the program if the pattern is not found.
+        """
+
+        regex_pattern = "([^/]+)$"
+        profile_name_payload = re.search(regex_pattern, self.url)
+        if profile_name_payload:
+            result = profile_name_payload.group(1)
+            self.__class_dict.setdefault("Profile Name", result)
+        else:
+            message = "Pattern not found in the URL"
+            self.__class_dict.update({"Message": message})
+            exit(self.__class_dict.get("Message"))
+
+    def profile_picture(self, soup: BeautifulSoup):
+        """Scrapes the user's profile picture from MyAnimeList.net and downloads it.
+
+        This method attempts to find the profile picture using the CSS selector `.lazyload`.
+        If the
+        image is found, it extracts the image URL from the `data-src` attribute and downloads the
+        image binary data using the `requests` library.
+        The downloaded image is then saved as a JPG
+        file named after the user's profile name in a folder named "images".
+
+        **Raises:**
+
+            Exception: If the profile picture is not found or the `data-src` attribute is missing.
+                     The error message is stored in the `_class_dict` attribute under the key "Message".
+        """
+
+        profile_picture_payload = soup.find("img", class_="lazyload")
+        if profile_picture_payload and "data-src" in profile_picture_payload.attrs:
+            image_link = profile_picture_payload["data-src"]
+            self.__class_dict.setdefault("Profile Picture Link", image_link)
+            image_binary_form = requests.get(image_link)
+            download_folder = "images"
+            os.makedirs(download_folder, exist_ok=True)
+            file_path = os.path.join(download_folder, f"{self.__class_dict['Profile Name']} Picture.jpg")
+            with open(file_path, "wb") as image:
+                image.write(image_binary_form.content)
+        else:
+            message = "Image not found or data-src attribute missing."
+            self.__class_dict.update({"Message": message})
+            exit(self.__class_dict.get("Message"))
+
+    def anime_list(self, soup: BeautifulSoup):
+        """Scrapes the user's anime list from MyAnimeList.net and saves the data to a JSON file.
+
+            This method first tries to find the link to the user's anime list using the CSS selector
+            `.btn-profile-submit.fl-l`.
+            If the link is found, it extracts data for each anime entry
+            including status, score,
+            number of watched episodes, title (English and Japanese),
+            airing status,
+            ID, genres, image path, start and end date strings.
+            This data is then saved to a JSON file
+            named after the user's profile name in a folder named "list data".
+
+            **Raises:**
+
+                Exception: If the anime list link is not found or the CSS selector is missing.
+                The error
+                         message is stored in the `_class_dict`
+                         attribute under the key "Message".
+            """
+
+        anime_list_link = soup.find("a", class_="btn-profile-submit fl-l")
+        if anime_list_link and "href" in anime_list_link.attrs:
+            list_link = anime_list_link["href"]
+            self.__class_dict.setdefault("Anime List", list_link)
+            self.scrape_anime_list()
+        else:
+            message = "Anime list link not found or 'class=btn-profile-submit fl-l' is missing"
+            self.__class_dict.update({"Message": message})
+            exit(self.__class_dict.get("Message"))
+
+    def scrape_anime_list(self):
+        """Helper function to scrape and save the user's anime list data.
+
+        This function iterates through pages of the user's anime list using API calls.
+        For each
+        retrieved page,
+        it extracts data for each anime entry using a predefined list of important tags.
+        The extracted data is then saved to a JSON file.
+
+        **Attributes:**
+
+            Important_tags (list):
+            A list of strings representing the keys to extract from each anime entry.
+            Export_folder (str): The name of the folder where the JSON file will be saved.
+            Defaults to "list data".
+            File_path (str): The path to the JSON file where the scraped data is saved.
+            Session (requests.Session): A session object used for making API requests.
+            List_api (str): The base URL for the API endpoint that provides the anime list data.
+            Json_offset (int): An offset value used for pagination while retrieving data.
+        """
+
+        important_tags = ["status", "score", "num_watched_episodes",
+                          "anime_title", "anime_title_eng", "anime_airing_status",
+                          "anime_id", "anime_score_val", "anime_popularity",
+                          "genres", "anime_image_path", "anime_start_date_string",
+                          "anime_end_date_string",
+                          ]
+        export_folder = "Anime List Data"
+        os.makedirs(export_folder, exist_ok=True)
+        file_path = os.path.join(export_folder, f"{self.__class_dict['Profile Name']}.json")
+        session = requests.Session()
+        json_offset = 0
+        list_api = "https://myanimelist.net/animelist/{name}/load.json?offset={json_offset}"
+
+        max_retries = 3  # Define the maximum number of retries for API requests
+        retries = 0
+
+        while True:
+            try:
+                response = session.get(list_api.format(name=self.__class_dict.get("Profile Name"),
+                                                       json_offset=json_offset))
+                response.raise_for_status()  # Raise an exception for non-200 status codes
+
+                json_data = response.json()
+                if not json_data:
+                    break
+                else:
+                    for item in json_data:
+                        extracted_data = {ke_y: item[ke_y] for ke_y in important_tags if ke_y in item}
+                        with open(file_path, "a") as json_file:
+                            json.dump(extracted_data, json_file)
+                            json_file.write("\n")
+            except requests.exceptions.RequestException as e:
+                # Handle request exceptions (e.g., connection errors, timeouts)
+                print(f"Error encountered while fetching data: {e}")
+                if retries < max_retries:
+                    retries += 1
+                    print(f"Retrying request (attempt {retries}/{max_retries})...")
+                    continue  # Retry the request
+                else:
+                    print(f"Maximum retries reached. Stopping scraping.")
+                    break  # Exit the loop after max retries
+            finally:
+                # Optional: Close session resources after successful completion or exception
+                session.close()
+
+            json_offset += 300
 
     def scraper(self):
         """ Scrapes profile information from a MyAnimeList.net user profile URL.
@@ -39,187 +203,20 @@ class MALProfile:
                        The specific status code is included in the exception message.
         """
 
-        _website_response = requests.get(self.url)
-        if _website_response:
-            self.__class_dict.setdefault("Status Code", _website_response.status_code)
-            soup = BeautifulSoup(_website_response.text, 'html.parser')
-
-            def profile_name():
-                """Extracts the profile name from the provided URL and stores it in the class dictionary.
-
-                This method attempts to extract the profile name using a regular expression pattern (`([^/]+)$`).
-                If the pattern matches the URL, it captures the last part (assumed to be the profile name)
-                and stores it in the class dictionary under the key "Profile Name".
-                If the pattern fails to match,
-                an error message indicating the pattern was not found
-                is stored in the dictionary under the key "Message".
-                The method then exits.
-
-                **Raises:**
-
-                    Exception:
-                    (Implicitly raised by `exit()` call) An exception exits the program if the pattern is not found.
-                """
-
-                regex_pattern = "([^/]+)$"
-                profile_name_payload = re.search(regex_pattern, self.url)
-                if profile_name_payload:
-                    result = profile_name_payload.group(1)
-                    self.__class_dict.setdefault("Profile Name", result)
-                else:
-                    message = "Pattern not found in the URL"
-                    self.__class_dict.update({"Message": message})
-                    exit(self.__class_dict.get("Message"))
-
-            def profile_picture():
-                """Scrapes the user's profile picture from MyAnimeList.net and downloads it.
-
-                This method attempts to find the profile picture using the CSS selector `.lazyload`.
-                If the
-                image is found, it extracts the image URL from the `data-src` attribute and downloads the
-                image binary data using the `requests` library.
-                The downloaded image is then saved as a JPG
-                file named after the user's profile name in a folder named "images".
-
-                **Raises:**
-
-                    Exception: If the profile picture is not found or the `data-src` attribute is missing.
-                             The error message is stored in the `_class_dict` attribute under the key "Message".
-                """
-
-                profile_picture_payload = soup.find("img", class_="lazyload")
-                if profile_picture_payload and "data-src" in profile_picture_payload.attrs:
-                    image_link = profile_picture_payload["data-src"]
-                    self.__class_dict.setdefault("Profile Picture Link", image_link)
-                    image_binary_form = requests.get(image_link)
-                    download_folder = "images"
-                    os.makedirs(download_folder, exist_ok=True)
-                    file_path = os.path.join(download_folder, f"{self.__class_dict['Profile Name']} Picture.jpg")
-                    with open(file_path, "wb") as image:
-                        image.write(image_binary_form.content)
-                else:
-                    message = "Image not found or data-src attribute missing."
-                    self.__class_dict.update({"Message": message})
-                    exit(self.__class_dict.get("Message"))
-
-            def anime_list():
-                """Scrapes the user's anime list from MyAnimeList.net and saves the data to a JSON file.
-
-                    This method first tries to find the link to the user's anime list using the CSS selector
-                    `.btn-profile-submit.fl-l`.
-                    If the link is found, it extracts data for each anime entry
-                    including status, score,
-                    number of watched episodes, title (English and Japanese),
-                    airing status,
-                    ID, genres, image path, start and end date strings.
-                    This data is then saved to a JSON file
-                    named after the user's profile name in a folder named "list data".
-
-                    **Raises:**
-
-                        Exception: If the anime list link is not found or the CSS selector is missing.
-                        The error
-                                 message is stored in the `_class_dict`
-                                 attribute under the key "Message".
-                    """
-
-                anime_list_link = soup.find("a", class_="btn-profile-submit fl-l")
-                if anime_list_link and "href" in anime_list_link.attrs:
-                    list_link = anime_list_link["href"]
-                    self.__class_dict.setdefault("Anime List", list_link)
-
-                    def scrape_list():
-                        """Helper function to scrape and save the user's anime list data.
-
-                        This function iterates through pages of the user's anime list using API calls.
-                        For each
-                        retrieved page,
-                        it extracts data for each anime entry using a predefined list of important tags.
-                        The extracted data is then saved to a JSON file.
-
-                        **Attributes:**
-
-                            Important_tags (list):
-                            A list of strings representing the keys to extract from each anime entry.
-                            Export_folder (str): The name of the folder where the JSON file will be saved.
-                            Defaults to "list data".
-                            File_path (str): The path to the JSON file where the scraped data is saved.
-                            Session (requests.Session): A session object used for making API requests.
-                            List_api (str): The base URL for the API endpoint that provides the anime list data.
-                            Json_offset (int): An offset value used for pagination while retrieving data.
-                        """
-
-                        important_tags = ["status", "score", "num_watched_episodes",
-                                          "anime_title", "anime_title_eng", "anime_airing_status",
-                                          "anime_id", "anime_score_val", "anime_popularity",
-                                          "genres", "anime_image_path", "anime_start_date_string",
-                                          "anime_end_date_string",
-                                          ]
-                        export_folder = "Anime List Data"
-                        os.makedirs(export_folder, exist_ok=True)
-                        file_path = os.path.join(export_folder, f"{self.__class_dict['Profile Name']}.json")
-                        session = requests.Session()
-                        json_offset = 0
-                        list_api = "https://myanimelist.net/animelist/{name}/load.json?offset={json_offset}"
-
-                        max_retries = 3  # Define the maximum number of retries for API requests
-                        retries = 0
-
-                        while True:
-                            try:
-                                response = session.get(list_api.format(name=self.__class_dict.get("Profile Name"),
-                                                                       json_offset=json_offset))
-                                response.raise_for_status()  # Raise an exception for non-200 status codes
-
-                                json_data = response.json()
-                                if not json_data:
-                                    break
-                                else:
-                                    for item in json_data:
-                                        extracted_data = {ke_y: item[ke_y] for ke_y in important_tags if ke_y in item}
-                                        with open(file_path, "a") as json_file:
-                                            json.dump(extracted_data, json_file)
-                                            json_file.write("\n")
-                            except requests.exceptions.RequestException as e:
-                                # Handle request exceptions (e.g., connection errors, timeouts)
-                                print(f"Error encountered while fetching data: {e}")
-                                if retries < max_retries:
-                                    retries += 1
-                                    print(f"Retrying request (attempt {retries}/{max_retries})...")
-                                    continue  # Retry the request
-                                else:
-                                    print(f"Maximum retries reached. Stopping scraping.")
-                                    break  # Exit the loop after max retries
-                            finally:
-                                # Optional: Close session resources after successful completion or exception
-                                session.close()
-
-                            json_offset += 300
-
-                    scrape_list()
-                else:
-                    message = "Anime list link not found or 'class=btn-profile-submit fl-l' is missing"
-                    self.__class_dict.update({"Message": message})
-                    exit(self.__class_dict.get("Message"))
-
-            profile_name()
-            profile_picture()
-            anime_list()
-        else:
-            raise Exception(f"Non-success status code: {_website_response.status_code}")
+        self.profile_name()
+        self.profile_picture(self.soup)
+        self.anime_list(self.soup)
 
     def get_class_dict(self) -> dict:
         return self.__class_dict
 
 
 if __name__ == "__main__":
-    # link = "https://myanimelist.net/profile/Paulternative"
     link = "https://myanimelist.net/profile/Rawwhite"
     # link = "https://myanimelist.net/profile/deatacitta"
+    # link = "https://myanimelist.net/profile/Paulternative"
     test = MALProfile(link)
     test.scraper()
     class_variables = test.get_class_dict()
-    # Accessing protected variable
     for key, value in class_variables.items():
-        # print(f"{key}: {value:^90}")
         print(f"{key}: {value}")
